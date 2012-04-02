752a4b5e1544a9aaf1a587aab401cbe94c285dfc
Jeff Tilson <jrtilson@gmail.com>
2012-04-02 12:53:27 -0400

    Initial Commit

---
+++ languages/en.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Blog Connector English Language Translation
+ *
+ * @package TGSBlogConnector
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Jeff Tilson
+ * @copyright THINK Global School 2010-2012
+ * @link http://www.thinkglobalschool.org/
+ * 
+ */
+
+$english = array(
+	// Page titles 
+
+	// Labels
+
+	// River
+
+	// Other content
+);
+
+add_translation('en',$english);
